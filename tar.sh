dc509c49e0da7668ecc456cb2a0fcfd0d1a8cc86
thecatvoid <voidcat@tutanota.com>
2022-06-09 21:30:30 +0530

    update

---
+++ tar.sh
@@ -0,0 +1,1 @@
+tar cpzf gentoo.tar.gz --exclude=/gentoo/proc --exclude=/gentoo/tmp --exclude=/gentoo/mnt --exclude=/gentoo/dev --exclude=/gentoo/sys --exclude=/gentoo/run --exclude=/gentoo/media --exclude=/gentoo/var/log --exclude=/gentoo/var/cache --exclude=/gentoo/home/ --one-file-system /gentoo
